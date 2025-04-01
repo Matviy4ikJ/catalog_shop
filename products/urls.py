308a1fdc04382cdaed6d0bbc93a7fb2fd3a78750
Matviy4ik <matvejbajdik1@gmail.com>
2025-04-01 19:31:15 +0300

    Filters, categories

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, about_us
+from .views import index, about_us, product_details
 
 urlpatterns = [
     path('', index, name='index'),
     path('about_us/', about_us, name='about_us'),
+    path('product/<int:product_id>/', product_details, name='product_details')
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from products.models import Product
+from products.models import Product, Category
 
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {"products": products})
+    categories = Category.objects.all()
+
+    category_name = request.GET.get('category')
+    filter_name = request.GET.get('filter')
+    product_name = request.GET.get('search')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    if product_name:
+        products = products.filter(name__icontains=product_name)
+
+    if category_name:
+        category = Category.objects.get(name=category_name)
+        products = products.filter(category=category)
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    match filter_name:
+        case "price_increase":
+            products = products.order_by("price")
+
+        case "price_decrease":
+            products = products.order_by("-price")
+
+        case "rating_increase":
+            products = products.order_by("rating")
+
+        case "rating_decrease":
+            products = products.order_by("-rating")
+
+        case "date_newest":
+            products = products.order_by("-created_at")
+
+        case "date_oldest":
+            products = products.order_by("created_at")
+
+    return render(
+        request,
+        "index.html",
+        {"products": products, "categories": categories},
+    )
 
 
 def about_us(request):
     return render(request, 'about_us.html')
+
+
+def product_details(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
